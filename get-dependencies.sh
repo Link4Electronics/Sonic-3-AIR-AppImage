2b3bce99d3eae09dcf77499629e4afb0db16dc9c
Link4Electronics <link4electronics@gmail.com>
2026-02-13 07:34:59 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,7 +6,11 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-# pacman -Syu --noconfirm PACKAGESHERE
+pacman -Syu --noconfirm \
+    libdecor       \
+    pipewire-audio \
+    pipewire-jack  \
+    sdl2
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
