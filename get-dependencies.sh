2c166f88348c4896dcf2b273cc76c473be9f06de
Link4Electronics <link4electronics@gmail.com>
2026-02-15 01:39:16 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -42,7 +42,7 @@ mkdir -p ./AppDir/bin/data
 #sed -i '113,125s|^|//|w /dev/stdout' Oxygen/sonic3air/source/sonic3air/client/crowdcontrol/CrowdControlClient.cpp
 #cd ..
 cd ./sonic3air
-patch -p1 < ./0001-fix-sdl-pipewire.patch
+patch -p1 < ../0001-fix-sdl-pipewire.patch
 cd Oxygen/sonic3air/build/_cmake
 #cd ./sonic3air/Oxygen/sonic3air/build/_cmake
 cmake . \
