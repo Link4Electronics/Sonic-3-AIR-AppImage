6fbba7aa0c00cb77aa6df0ec7de1f882b9356ad4
Link4Electronics <link4electronics@gmail.com>
2026-03-04 03:36:16 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -26,7 +26,7 @@ if [ "${ARCH}" = x86_64 ]; then
     echo "Dowload last stable build of Sonic-3-AIR for Linux..."
     echo "---------------------------------------------------------------"
     VERSION=v26.02.28.0
-    wget https://github.com/Eukaryot/sonic3air/releases/download/$VERSION-test/sonic3air_game.tar.gz
+    wget https://github.com/Eukaryot/sonic3air/releases/download/$VERSION-preview/sonic3air_game.tar.gz
     echo "$VERSION" > ~/version
 
     bsdtar -xvf sonic3air_game.tar.gz
@@ -50,7 +50,7 @@ else
     #VERSION=$(git ls-remote --tags --sort="v:refname" "$REPO" | grep -v "\^{}" | tail -n1 | sed 's|.*/||')
     #git clone --branch "$VERSION" --single-branch "$REPO" ./sonic3air
     VERSION=v26.02.28.0
-    git clone --branch $VERSION-test --single-branch "$REPO" ./sonic3air
+    git clone --branch $VERSION-preview --single-branch "$REPO" ./sonic3air
     echo "$VERSION" > ~/version
 
     mkdir -p ./AppDir/bin/data
